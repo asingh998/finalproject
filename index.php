72b37494881236a8cf5813b833df8b149fd8cc7f
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-06-10 19:48:22 -0400

    added summary routing

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ error_reporting(E_ALL);
 //Require the autoload file
 require_once("vendor/autoload.php");
 
+//Start a session
+session_start();
+
 //Instantiate the F3 Base class
 $f3 = Base::instance();
 
@@ -20,10 +23,25 @@ $f3->route('GET /', function () {
 
 //form 1
 
-$f3->route('GET /form', function()
+$f3->route('GET|POST /form', function($f3)
 {
     //echo '<h1>form</h1>';
+    //If the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        var_dump($_POST);
 
+        //Data is valid
+        if (empty($f3->get('errors'))) {
+
+            //Store the data in the session array
+            $_SESSION['first'] = $_POST['first'];
+
+            //Redirect to Order 2 page
+            $f3->reroute('summary');
+        }
+    }
+
+    $f3->set('first', $_POST['first']);
     $view = new Template();
     echo $view->render('views/form.html');
 });
@@ -35,6 +53,15 @@ $f3->route('GET /form2', function()
 
     $view = new Template();
     echo $view->render('views/form2.html');
+});
+
+//summary
+$f3->route('GET /summary', function()
+{
+    //echo '<h1>form</h1>';
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 //Run F3
